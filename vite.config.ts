a191569b6f302c69d200ee54b7565d7aac3f38de
carnold84 <carnold84@users.noreply.github.com>
2025-02-15 19:55:21 +1000

    Added CSS nesting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import postcssNesting from "postcss-nesting";
+import { defineConfig } from "vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -12,6 +13,11 @@ export default defineConfig(async () => ({
   //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
+  css: {
+    postcss: {
+      plugins: [postcssNesting],
+    },
+  },
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
